8f3bd56da4f8ab55af581f970788eb39dc6eba3a
JimmyShi22 <417711026@qq.com>
2019-10-09 11:21:47 +0800

    update ci check

--- .ci/ci_check.sh
+++ .ci/ci_check.sh
@@ -8,8 +8,8 @@ echo "127.0.0.1:4 agency1 1,2,3" > ipconf
 ./build_chain.sh -e bin/fisco-bcos -f ipconf -p 30300,20200,8545 -v 2.1.0
 ./nodes/127.0.0.1/start_all.sh
 ./nodes/127.0.0.1/fisco-bcos -v
-cp nodes/127.0.0.1/sdk/* src/integration-test/resources/
-mv src/integration-test/resources/applicationContext-sample.xml src/integration-test/resources/applicationContext.xml
+# cp nodes/127.0.0.1/sdk/* src/integration-test/resources/
+# mv src/integration-test/resources/applicationContext-sample.xml src/integration-test/resources/applicationContext.xml
 ./gradlew verifyGoogleJavaFormat
 ./gradlew build
 ./gradlew test
