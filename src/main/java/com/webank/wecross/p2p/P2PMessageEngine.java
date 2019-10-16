1d27a41975a727a519bd15f364d33539f0b62015
JimmyShi22 <417711026@qq.com>
2019-10-16 11:11:53 +0800

    delete register callback logic

--- src/main/java/com/webank/wecross/p2p/P2PMessageEngine.java
+++ src/main/java/com/webank/wecross/p2p/P2PMessageEngine.java
@@ -3,16 +3,7 @@ package com.webank.wecross.p2p;
 import java.util.Map;
 
 public abstract class P2PMessageEngine {
-    private Map<String, P2PMessageCallback> registeredHandler;
-
     public abstract <T> void asyncSendMessage(
             Peer peer, P2PMessage<T> msg, P2PMessageCallback callback);
 
-    public synchronized void registerHandler(String type, P2PMessageCallback handler)
-            throws Exception {
-        if (registeredHandler.containsKey(type)) {
-            throw new Exception("Duplicate register handler: " + type);
-        }
-        registeredHandler.put(type, handler);
-    }
 }
