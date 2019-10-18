04dd73fd65aa9c7b53da5cf9a60b5d8abf8ed964
JimmyShi22 <417711026@qq.com>
2019-10-18 18:41:42 +0800

    merge from remote

--- src/main/java/com/webank/wecross/core/SeqUtils.java
+++ src/main/java/com/webank/wecross/core/SeqUtils.java
@@ -4,7 +4,7 @@ import java.security.SecureRandom;
 
 public class SeqUtils {
     static final int SEQ_BOUND = Integer.MAX_VALUE - 1;
-    
+
     public static synchronized int newSeq() {
         SecureRandom rand = new SecureRandom();
         return rand.nextInt(SEQ_BOUND);
--- src/main/java/com/webank/wecross/host/PeerManager.java
+++ src/main/java/com/webank/wecross/host/PeerManager.java
@@ -31,7 +31,7 @@ public class PeerManager {
 
     public void start() {
         newSeq();
-        // broadcastSeqRequest();
+        broadcastSeqRequest();
     }
 
     public void newSeq() {
