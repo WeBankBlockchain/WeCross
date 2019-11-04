2b06cbb989e48fd32c2630e155d1de936e5fef42
jimmyshi <417711026@qq.com>
2019-11-04 15:13:18 +0800

    Rename peer with peerInfo (#49)
    * rename peer with peerInfo
    
    * format code

--- src/main/java/com/webank/wecross/p2p/Peer.java
+++ src/main/java/com/webank/wecross/p2p/Peer.java
@@ -1,14 +1,8 @@
 package com.webank.wecross.p2p;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Peer {
     private String url;
     private String name;
-    private int seq = 1;
-    private Set<String> resources = new HashSet<>();
-    private long lastActiveTimestamp = System.currentTimeMillis();
 
     public Peer() {}
 
@@ -33,37 +27,8 @@ public class Peer {
         this.name = name;
     }
 
-    public int getSeq() {
-        return seq;
-    }
-
-    public void setSeq(int seq) {
-        this.seq = seq;
-    }
-
-    public Set<String> getResources() {
-        return resources;
-    }
-
-    public synchronized void setResources(int seq, Set<String> resources) {
-        this.setSeq(seq);
-        this.resources = resources;
-    }
-
-    public void noteAlive() {
-        this.lastActiveTimestamp = System.currentTimeMillis();
-    }
-
-    public boolean isTimeout(long timeout) {
-        return (System.currentTimeMillis() - lastActiveTimestamp) > timeout;
-    }
-
     @Override
     public String toString() {
         return "(name:" + name + ", url:" + url + ")";
-    }
-
-    public long getLastActiveTimestamp() {
-        return lastActiveTimestamp;
     }
 }
