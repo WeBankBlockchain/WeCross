ccfecb78fad69d7bfec1a05290a9480a51e31f0b
JimmyShi22 <417711026@qq.com>
2019-11-21 11:21:57 +0800

    Add service runner in test

---
+++ src/test/java/com/webank/wecross/test/ServiceRunner.java
@@ -0,0 +1,9 @@
+package com.webank.wecross.test;
+
+import com.webank.wecross.Service;
+
+public class ServiceRunner {
+    public static void main(String[] args) {
+        Service.main(args);
+    }
+}
