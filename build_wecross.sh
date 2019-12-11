6d7fc24576ea9a431e7604e9e6cb7480709f21d1
shareong <740310627@qq.com>
2019-12-11 20:53:28 +0800

    add scripts to build wecross and to create stubs configuration

---
+++ build_wecross.sh
@@ -0,0 +1,228 @@
+#!/bin/bash
+
+set -e
+
+counter=0
+network=
+ip_array=
+rpc_port_array=
+p2p_port_array=
+peers_array=
+need_help=1
+
+# shellcheck disable=SC2120
+help()
+{
+    echo "$1"
+    cat << EOF
+Usage:
+    -i [Network ID] [IP] [Port] [Port]     Init wecross project by wecross network id, ip, rpc_port and p2p_port, e.g: payment 127.0.0.1 11100 11200
+    -f [Network ID] [File]                 Init wecross project by wecross network id and ip&ports file. file should be splited by line "ip rpc_port p2p_port" e.g: 127.0.0.1 11100 11200
+    -h                                     Call for help
+e.g
+    bash $0 -i payment 127.0.0.1 11100 11200
+EOF
+exit 0
+}
+
+check_env() {
+    # shellcheck disable=SC2143
+    # shellcheck disable=SC2236
+    [ ! -z "$(openssl version | grep 1.0.2)" ] || [ ! -z "$(openssl version | grep 1.1)" ] || [ ! -z "$(openssl version | grep reSSL)" ] || {
+        echo "please install openssl!"
+        #echo "download openssl from https://www.openssl.org."
+        echo "use \"openssl version\" command to check."
+        exit 1
+    }
+    if [ ! -z "$(openssl version | grep reSSL)" ];then
+        export PATH="/usr/local/opt/openssl/bin:$PATH"
+    fi
+    if [ "$(uname)" == "Darwin" ];then
+        macOS="macOS"
+    fi
+    if [ "$(uname -m)" != "x86_64" ];then
+        x86_64_arch="false"
+    fi
+}
+
+exec_command()
+{
+while getopts "i:f:h" option;do
+    # shellcheck disable=SC2220
+    case ${option} in
+    i)
+        check_params "i" $#
+        need_help=0
+        network="${2}"
+        build_project
+        gen_crt 1
+        gen_one_wecross 0 "${3}" "${4}" "${5}"
+        finish_jod
+    ;;
+    f)
+        check_params "f" $#
+        need_help=0
+        network="${2}"
+        parse_ip_file "${3}"
+        build_project
+        gen_wecross_tars
+        finish_jod
+    ;;
+    h)  help;;
+    esac
+done
+}
+
+check_params()
+{
+    # shellcheck disable=SC2170
+    if [ "${1}" == "i" ];then
+        if [ "${2}" -ne 5 ];then
+            echo -e "\033[31mIllegal number of parameters, -h for help!\033[0m"
+            exit 1
+        fi
+    elif [ "${1}" == "f" ];then
+        if [ "${2}" -ne 3 ];then
+            echo -e "\033[31mIllegal number of parameters, -h for help!\033[0m"
+            exit 1
+        fi
+    fi
+}
+
+build_project()
+{
+    if [ -d WeCross ];then
+        echo -e "\033[31mWeCross: File exists, please change directory to run this script\033[0m"
+        exit 1
+    fi
+    git clone https://github.com/WeBankFinTech/WeCross.git
+    cd WeCross
+    ./gradlew assemble 2>&1 | tee output.log
+    # shellcheck disable=SC2046
+    # shellcheck disable=SC2006
+    if [ `grep -c "BUILD SUCCESSFUL" output.log` -eq '0' ]; then
+        echo -e "\033[31mBuild Wecross project failed \033[0m"
+        echo -e "\033[31mSee output.log for details \033[0m"
+        mv output.log ../output.log
+        cd ..
+        exit 1
+    fi
+    cd ..
+}
+
+gen_crt()
+{
+    cd WeCross
+    # get ca.crt
+    bash scripts/create_cert.sh -c
+    # get node.crt by number
+    bash scripts/create_cert.sh -n -C "${1}"
+    cd ..
+}
+
+#index ip rpc_port p2p_port pers
+gen_one_wecross()
+{
+    cd WeCross
+    output=${2}-${3}-${4}
+    cp -r dist "${output}"
+    cp -r node"${1}" "${output}"/conf/p2p
+    gen_conf "${output}"/conf/wecross.toml "${2}" "${3}" "${4}" "${5}"
+    cp -r "${output}" ../
+    cd ..
+}
+
+gen_one_wecross_tar()
+{
+    cd WeCross
+    output=${2}-${3}-${4}
+    cp -r dist "${output}"
+    cp -r node"${1}" "${output}"/conf/p2p
+    gen_conf "${output}"/conf/wecross.toml "${2}" "${3}" "${4}" "${5}"
+    tar -czf ../"${output}".tar.gz "${output}"
+    cd ..
+    echo -e "\033[32mCreate ${output}.tar.gz  successfully \033[0m"
+}
+
+gen_conf()
+{
+    cat << EOF > "${1}"
+[common]
+    network = '${network}'
+    visible = true
+
+[stubs]
+    path = 'classpath:stubs'
+
+[server] # tomcat server
+    address = '${2}'
+    port = ${3}
+
+[p2p]
+    listenIP = '0.0.0.0'
+    listenPort = ${4}
+    caCert = 'classpath:p2p/ca.crt'
+    sslCert = 'classpath:p2p/node.crt'
+    sslKey = 'classpath:p2p/node.key'
+    peers = [${5}]
+
+[test]
+    enableTestResource = true
+EOF
+}
+
+parse_ip_file()
+{
+    # shellcheck disable=SC2162
+    while read line;do
+        ip_array[counter]=$(echo "${line}" | awk '{print $1}')
+        rpc_port_array[counter]=$(echo "${line}" | awk '{print $2}')
+        p2p_port_array[counter]=$(echo "${line}" | awk '{print $3}')
+        if [ -z "${ip_array[counter]}" -o -z "${rpc_port_array[counter]}" -o -z "${p2p_port_array[counter]}" ];then
+            echo -e "\033[31mPlease check ${1}, make sure there is no empty line!\033[0m"
+            exit 1
+        fi
+        ((++counter))
+    done < "${1}"
+}
+
+# shellcheck disable=SC2120
+gen_wecross_tars()
+{
+    gen_crt "${counter}"
+
+    for ((i=0;i<counter;i++))
+    do
+        for((j=0;j<counter;j++))
+        do
+            # shellcheck disable=SC2057
+            if [ ${i} -ne ${j} ];then
+                peers_array[i]=${peers_array[i]}"'"${ip_array[j]}:${p2p_port_array[j]}"'",
+            fi
+        done
+
+        peers_array[i]=$(echo "${peers_array[i]}" | awk '{sub(/.$/,"")}1')
+        gen_one_wecross_tar "${i}" "${ip_array[i]}" "${rpc_port_array[i]}" "${p2p_port_array[i]}" "${peers_array[i]}"
+    done
+}
+
+finish_jod()
+{
+    if [ -d WeCross ];then
+        rm -rf WeCross
+    fi
+    echo -e "\033[32mBuild Wecross successfully \033[0m"
+}
+
+print_help()
+{
+    # shellcheck disable=SC2236
+    if [ ${need_help} -eq 1 ];then
+        help
+    fi
+}
+
+check_env
+# shellcheck disable=SC2068
+exec_command $@
+print_help
---
+++ scripts/create_stubs_config.sh
@@ -0,0 +1,261 @@
+#!/bin/bash
+
+set -e
+
+BCOS_TYPE=BCOS
+JD_TYPE=JD
+FABRIC_TYPE=FABRIC
+pem_file=
+param_arry=
+need_help=1
+# shellcheck disable=SC2120
+help()
+{
+    echo "$1"
+    cat << EOF
+Usage:
+    -a [Root Dir] [[Stub type] [Stub name]]   Generate stub configuration by lisf of type and name
+                                              Supported types: BCOS, FABRIC, JD
+    -b [Root Dir] [Stub name]                 Generate BCOS stub configuration
+    -f [Root Dir] [Stub name]                 Generate FABRIC stub configuration
+    -j [Root Dir] [Stub name]                 Generate JD stub configuration
+    -h                                        Call for help
+e.g
+    bash $0 -d stubs BCOS bcosChain FABRIC fabricChain
+EOF
+exit 0
+}
+
+exec_command()
+{
+while getopts "a:b:f::jh" option;do
+    # shellcheck disable=SC2220
+    case ${option} in
+    a)
+        need_help=0
+        check_params "a" $#
+        mkdir -p conf/"${2}"
+        create_stubs "$*" $#
+    ;;
+    b)
+        need_help=0
+        check_params "b" $#
+        mkdir -p conf/"${2}"
+        create_bcos_stub "${2}" "${3}"
+    ;;
+    f)
+        need_help=0
+        check_params "f" $#
+        mkdir -p conf/"${2}"
+        create_fabric_stub "${2}" "${3}"
+    ;;
+    j)
+        need_help=0
+        check_params "j" $#
+        mkdir -p conf/"${2}"
+        create_jd_stub "${2}" "${3}"
+    ;;
+    h)  help;;
+    esac
+done
+}
+
+check_params()
+{
+    # shellcheck disable=SC2170
+    if [ "${1}" == "a" ];then
+        # shellcheck disable=SC2046
+        # shellcheck disable=SC2006
+        # shellcheck disable=SC2003
+        if [ `expr "${2}" % 2` -ne 0 ];then
+            echo -e "\033[31mIllegal number of parameters, -h for help!\033[0m"
+            exit 1
+        fi
+
+    elif [ "${1}" == "b" ] || [ "${1}" == "f" ] || [ "${1}" == "j" ]; then
+        if [ "${2}" -ne 3 ];then
+            echo -e "\033[31mIllegal number of parameters, -h for help!\033[0m"
+            exit 1
+        fi
+    fi
+}
+
+
+
+create_stubs()
+{
+    # shellcheck disable=SC2206
+    param_arry=(${1})
+    for ((i=2;i<${2};))
+    do
+        if [ "${param_arry[i]}" == ${BCOS_TYPE} ];then
+            create_bcos_stub "${param_arry[1]}" "${param_arry[i+1]}"
+        elif [ "${param_arry[i]}" == ${JD_TYPE} ]; then
+            create_jd_stub "${param_arry[1]}" "${param_arry[i+1]}"
+        elif [ "${param_arry[i]}" == ${FABRIC_TYPE} ]; then
+            create_fabric_stub "${param_arry[1]}" "${param_arry[i+1]}"
+        else
+            echo -e "\033[31mUnsupported stub type: ${param_arry[i]}\033[0m"
+            exit 1
+        fi
+        i=${i}+2
+    done
+    echo -e "\033[32mCreate stubs configuration successfully \033[0m"
+}
+
+create_bcos_stub()
+{
+    mkdir -p conf/"${1}"/"${2}"
+    gen_bcos_account conf/"${1}"/"${2}"
+    cat << EOF > conf/"${1}"/"${2}"/stub.toml
+[common]
+    stub = '${2}' # stub must be same with directory name
+    type = 'BCOS'
+
+[smCrypto]
+    # boolean
+    enable = false
+
+[account]
+    accountFile = 'classpath:/${1}/${2}/${pem_file}'
+    password = ''  # if you choose .p12, then password is required
+
+
+[channelService]
+    timeout = 60000  # millisecond
+    caCert = 'classpath:/${1}/${2}/ca.crt'
+    sslCert = 'classpath:/${1}/${2}/sdk.crt'
+    sslKey = 'classpath:/${1}/${2}/sdk.key'
+    groupId = 1
+    connectionsStr = ['127.0.0.1:20200']
+
+# resources is a list
+[[resources]]
+    # name cannot be repeated
+    name = 'HelloWorldContract'
+    type = 'BCOS_CONTRACT'
+    contractAddress = '0x8827cca7f0f38b861b62dae6d711efe92a1e3602'
+[[resources]]
+    name = 'FirstTomlContract'
+    type = 'BCOS_CONTRACT'
+    contractAddress = '0x584ecb848dd84499639fbe2581bfb8a8774b485c
+EOF
+
+    echo -e "\033[32mCreate ${1}/${2}/stub.toml successfully \033[0m"
+}
+
+gen_bcos_account()
+{
+    expect <<EOF
+    spawn bash create_bcos_account.sh
+    expect "Password"
+    send "123456\r"
+    expect "Password"
+    send "123456\r"
+    expect eof
+EOF
+
+    rm accounts/*.public.*
+    cd accounts
+    # shellcheck disable=SC2035
+    # shellcheck disable=SC2012
+    pem_file=$(ls *.pem | awk -F'.' '{print $0}')
+    cd ..
+    cp accounts/"${pem_file}" "${1}"
+    rm -rf accounts
+}
+
+create_jd_stub()
+{
+    mkdir -p conf/"${1}"/"${2}"
+
+   cat << EOF > conf/"${1}"/"${2}"/stub.toml
+[common]
+    stub = '${2}' # stub must be same with directory name
+    type = 'JDCHAIN'
+
+# jdServices is a list
+[[jdServices]]
+     privateKey = '0000000000000000'
+     publicKey = '111111111111111'
+     password = '222222222222222'
+     connectionsStr = '127.0.0.1:18081'
+[[jdServices]]
+     privateKey = '0000000000000000'
+     publicKey = '111111111111111'
+     password = '222222222222222'
+     connectionsStr = '127.0.0.1:18082'
+
+# resources is a list
+[[resources]]
+    # name cannot be repeated
+    name = 'HelloWorldContract'
+    type = 'JDCHAIN_CONTRACT'
+    contractAddress = '0x38735ad749aebd9d6e9c7350ae00c28c8903dc7a'
+[[resources]]
+    name = 'FirstTomlContract'
+    type = 'JDCHAIN_CONTRACT'
+    contractAddress = '0x38735ad749aebd9d6e9c7350ae00c28c8903dc7a'
+EOF
+
+    echo -e "\033[32mCreate ${1}/${2}/stub.toml successfully \033[0m"
+}
+
+create_fabric_stub()
+{
+    mkdir -p conf/"${1}"/"${2}"
+
+   cat << EOF > conf/"${1}"/"${2}"/stub.toml
+[common]
+    stub = '${2}' # stub must be same with directory name
+    type = 'FABRIC'
+
+# fabricServices is a list
+[fabricServices]
+     channelName = 'mychannel'
+     orgName = 'Org1'
+     mspId = 'Org1MSP'
+     orgUserName = 'Admin'
+     orgUserKeyFile = 'classpath:/${1}/${2}/5895923570c12e5a0ba4ff9a908ed10574b475797b1fa838a4a465d6121b8ddf_sk'
+     orgUserCertFile = 'classpath:/${1}/${2}/Admin@org1.example.com-cert.pem'
+     ordererTlsCaFile = 'classpath:/${1}/${2}/tlsca.example.com-cert.pem'
+     ordererAddress = 'grpcs://127.0.0.1:7050'
+
+[peers]
+    [peers.a]
+        peerTlsCaFile = 'classpath:/${1}/${2}/tlsca.org1.example.com-cert.pem'
+        peerAddress = 'grpcs://127.0.0.1:7051'
+    [peers.b]
+         peerTlsCaFile = 'classpath:/${1}/${2}/tlsca.org2.example.com-cert.pem'
+         peerAddress = 'grpcs://127.0.0.1:9051'
+
+# resources is a list
+[[resources]]
+    # name cannot be repeated
+    name = 'HelloWorldContract'
+    type = 'FABRIC_CONTRACT'
+    chainCodeName = 'mycc'
+    chainLanguage = "go"
+    peers=['a','b']
+[[resources]]
+    name = 'FirstTomlContract'
+    type = 'FABRIC_CONTRACT'
+    chainLanguage = "go"
+    chainCodeName = 'cscc'
+    peers=['a','b']
+EOF
+
+    echo -e "\033[32mCreate ${1}/${2}/stub.toml successfully \033[0m"
+}
+
+print_help()
+{
+    # shellcheck disable=SC2236
+    if [ ${need_help} -eq 1 ];then
+        help
+    fi
+}
+
+# shellcheck disable=SC2068
+exec_command $@
+print_help
