0cf8e982e4bb6345de1e1a363ab0188543233bf8
shareong <740310627@qq.com>
2019-12-13 16:03:17 +0800

    Update default ports and improve the scripts' prompt

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -6,7 +6,7 @@ APPS_FOLDER=$(pwd)/apps
 
 wecross_pid=$(ps -ef | grep com.webank.wecross.Service | grep ${APPS_FOLDER} | grep -v grep | awk '{print $2}')
 if [ -z ${wecross_pid} ]; then
-    echo -e "\033[31m Wecross isn't running \033[0m"
+    echo -e "\033[31mWeCross isn't running \033[0m"
     exit 0
 fi
 
@@ -14,10 +14,10 @@ ps -ef | grep com.webank.wecross.Service | grep ${APPS_FOLDER} | grep -v grep | 
 
 wecross_pid=$(ps -ef | grep com.webank.wecross.Service | grep ${APPS_FOLDER} | grep -v grep | awk '{print $2}')
 if [ -z ${wecross_pid} ]; then
-    echo -e "\033[32m Stop Wecross successfully \033[0m"
+    echo -e "\033[32mStop WeCross successfully \033[0m"
     exit 0
 fi
 sleep 0.5
 
-echo -e "\033[31m Exceed waiting time. Please try again to stop WeCross \033[0m"
+echo -e "\033[31mExceed waiting time. Please try again to stop WeCross \033[0m"
 exit 1
