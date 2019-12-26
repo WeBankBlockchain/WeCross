cb764997da517413880f4506e2abb28b81ad02be
JimmyShi22 <417711026@qq.com>
2019-12-26 11:44:27 +0800

    suport lower version

--- src/main/java/com/webank/wecross/restserver/Versions.java
+++ src/main/java/com/webank/wecross/restserver/Versions.java
@@ -2,7 +2,7 @@ package com.webank.wecross.restserver;
 
 public class Versions {
 
-    private static final String versionList[] = {"1"};
+    private static final String versionList[] = {"0.1", "0.2", "1"};
 
     public static String currentVersion = "1";
 
