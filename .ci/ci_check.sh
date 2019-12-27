0e0090832cbb27b697d790162d351a43f063b257
jimmyshi <417711026@qq.com>
2019-12-27 15:39:52 +0800

    fix scripts bugs (#125)
    * fix scripts bugs
    
    * fix ci check

--- .ci/ci_check.sh
+++ .ci/ci_check.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-curl -LO https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/dev/tools/build_chain.sh && chmod u+x build_chain.sh
-bash <(curl -s https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/dev/tools/ci/download_bin.sh) -b dev
+curl -LO https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/master/tools/build_chain.sh && chmod u+x build_chain.sh
+bash <(curl -s https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/master/tools/ci/download_bin.sh)
 echo "127.0.0.1:4 agency1 1,2,3" >ipconf
 ./build_chain.sh -e bin/fisco-bcos -f ipconf -p 30300,20200,8545 -v 2.1.0
 ./nodes/127.0.0.1/start_all.sh
