0cf54d0874faccdc9005b4043e4f3d5d268574cc
JimmyShi22 <417711026@qq.com>
2019-12-27 15:20:35 +0800

    fix scripts bugs

--- scripts/create_bcos_stub_config.sh
+++ scripts/create_bcos_stub_config.sh
@@ -100,7 +100,7 @@ EOF
         cd accounts
         # shellcheck disable=SC2035
         # shellcheck disable=SC2012
-        rm *.public.*
+        rm -f *.public.*
         p12_file=$(echo ./*.p12 | awk -F'.' '{print $0}')
         cd ..
         cp accounts/"${p12_file}" "${conf_dir}"/"${root_dir}"/"${stub_name}"
@@ -109,7 +109,7 @@ EOF
         cd accounts
         # shellcheck disable=SC2035
         # shellcheck disable=SC2012
-        rm *.public.*
+        rm  -f *.public.*
         pem_file=$(echo ./*.pem | awk -F'.' '{print $0}')
         cd ..
         cp accounts/"${pem_file}" "${conf_dir}"/"${root_dir}"/"${stub_name}"
