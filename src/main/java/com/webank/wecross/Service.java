270d8238a826868b3385802ed4ebca21fa08a7e3
MO NAN <651932351@qq.com>
2020-03-30 16:37:37 +0800

    remove bc (#175)
    * use bc as default security provider
    
    * remove bc
    
    * update format
    
    Co-authored-by: ancelmo <ancelmo@ANCELMOOA2.webank.com>

--- src/main/java/com/webank/wecross/Service.java
+++ src/main/java/com/webank/wecross/Service.java
@@ -1,7 +1,5 @@
 package com.webank.wecross;
 
-import java.security.Security;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -11,8 +9,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class Service {
 
     public static void main(String[] args) {
-        Security.insertProviderAt(new BouncyCastleProvider(), 1);
-
         SpringApplication.run(Service.class, args);
     }
 }
