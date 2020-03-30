f7a65c4c2993cf11fdf41f01da7ca52d3357fa37
JimmyShi22 <417711026@qq.com>
2020-03-30 14:46:38 +0800

    Upload source code during gradle publish

--- src/main/java/com/webank/wecross/stub/StubManager.java
+++ src/main/java/com/webank/wecross/stub/StubManager.java
@@ -5,13 +5,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StubManager {
-    private Map<String, StubFactory> drivers = new HashMap<String, StubFactory>();
+    private Map<String, com.webank.wecross.stub.StubFactory> drivers = new HashMap<>();
 
-    public void addStubFactory(String type, StubFactory stubFactory) {
+    public void addStubFactory(String type, com.webank.wecross.stub.StubFactory stubFactory) {
         drivers.put(type, stubFactory);
     }
 
-    public StubFactory getStubFactory(String type) throws WeCrossException {
+    public com.webank.wecross.stub.StubFactory getStubFactory(String type) throws WeCrossException {
         if (!drivers.containsKey(type)) {
             throw new WeCrossException(-1, "StubFactory[" + type + "] not found!");
         }
@@ -19,11 +19,11 @@ public class StubManager {
         return drivers.get(type);
     }
 
-    public Map<String, StubFactory> getDrivers() {
+    public Map<String, com.webank.wecross.stub.StubFactory> getDrivers() {
         return drivers;
     }
 
-    public void setDrivers(Map<String, StubFactory> drivers) {
+    public void setDrivers(Map<String, com.webank.wecross.stub.StubFactory> drivers) {
         this.drivers = drivers;
     }
 }
