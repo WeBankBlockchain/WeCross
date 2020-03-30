6095bd1ffc2ef4a6c77eb54de1d752bc24ccf55c
ancelmo <ancelmo@ANCELMOOA2.webank.com>
2020-03-30 10:35:45 +0800

    use bc as default security provider

--- src/main/java/com/webank/wecross/Service.java
+++ src/main/java/com/webank/wecross/Service.java
@@ -1,5 +1,7 @@
 package com.webank.wecross;
 
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,6 +11,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class Service {
 
     public static void main(String[] args) {
+        Security.insertProviderAt(new BouncyCastleProvider(), 1);
+
         SpringApplication.run(Service.class, args);
     }
 }
