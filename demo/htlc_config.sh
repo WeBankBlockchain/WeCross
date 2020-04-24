c861b3bb7eb5723991c9b7b0661c089d9c6b29fa
Shareong <740310627@qq.com>
2020-04-24 11:12:17 +0800

    update scripts of demo && add thread-config for p2p and https (#206)
    * update scripts of demo
    
    * add thread-config for p2p and https

--- demo/htlc_config.sh
+++ demo/htlc_config.sh
@@ -189,14 +189,10 @@ main()
     copy_console
     restart_router
 
-    LOG_INFO "Config htlc successfully!"
-        echo -e "
-
-Now, You can deal a cross-chain transfer by WeCross-Console using following command!
-
-BCOS   user: newHTLCTransferProposal payment.bcos.htlc bcos_sender bea2dfec011d830a86d0fbeeb383e622b576bb2c15287b1a86aacdba0a387e11 9dda9a5e175a919ee98ff0198927b0a765ef96cf917144b589bb8e510e04843c true 0x55f934bcbe1e9aef8337f5551142a442fdde781c 0x2b5ad5c4795c026514f8317c7a215e218dccd6cf 700 2000010000 Admin@org1.example.com User1@org1.example.com 500 2000000000
-
-Fabric user: newHTLCTransferProposal payment.fabric.htlc fabric_admin bea2dfec011d830a86d0fbeeb383e622b576bb2c15287b1a86aacdba0a387e11 null false 0x55f934bcbe1e9aef8337f5551142a442fdde781c 0x2b5ad5c4795c026514f8317c7a215e218dccd6cf 700 2000010000 Admin@org1.example.com User1@org1.example.com 500 2000000000
+    LOG_INFO "Config htlc successfully!\n"
+    LOG_INFO "Now, you can make a cross-chain transfer by WeCross console using following command!\n"
+    echo -e "[BCOS  user ]: newHTLCTransferProposal payment.bcos.htlc bcos_sender bea2dfec011d830a86d0fbeeb383e622b576bb2c15287b1a86aacdba0a387e11 9dda9a5e175a919ee98ff0198927b0a765ef96cf917144b589bb8e510e04843c true 0x55f934bcbe1e9aef8337f5551142a442fdde781c 0x2b5ad5c4795c026514f8317c7a215e218dccd6cf 700 2000010000 Admin@org1.example.com User1@org1.example.com 500 2000000000
+\n[Fabric user]: newHTLCTransferProposal payment.fabric.htlc fabric_admin bea2dfec011d830a86d0fbeeb383e622b576bb2c15287b1a86aacdba0a387e11 null false 0x55f934bcbe1e9aef8337f5551142a442fdde781c 0x2b5ad5c4795c026514f8317c7a215e218dccd6cf 700 2000010000 Admin@org1.example.com User1@org1.example.com 500 2000000000
 "
 }
 
