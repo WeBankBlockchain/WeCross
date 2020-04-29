970b50ef56634d2c63c5eadf53c5632ef9f0a8ca
JimmyShi22 <417711026@qq.com>
2020-04-29 15:34:25 +0800

    add console check

--- .ci/ci_demo_check.sh
+++ .ci/ci_demo_check.sh
@@ -10,15 +10,20 @@ cd demo
 
 # demo test
 bash build.sh <<EOF
-    Y
-    listResources
-    listAccounts
-    call payment.bcos.HelloWeCross bcos_user1 get
-    sendTransaction payment.bcos.HelloWeCross bcos_user1 set Tom
-    call payment.fabric.abac fabric_user1 query a
-    sendTransaction payment.fabric.abac fabric_user1 invoke a b 10
-    quit
+n
 EOF
+
+cd WeCross-Console/
+bash start.sh <<EOF
+listResources
+listAccounts
+call payment.bcos.HelloWeCross bcos_user1 get
+sendTransaction payment.bcos.HelloWeCross bcos_user1 set Tom
+call payment.fabric.abac fabric_user1 query a
+sendTransaction payment.fabric.abac fabric_user1 invoke a b 10
+quit
+EOF
+cd ..
 
 # htlc test
 bash htlc_config.sh
