9e9ebeb2e8211807a2335531faa41ae1522abb53
ancelmo <ancelmo@host.docker.internal>
2020-06-15 10:49:14 +0800

    remove stackPrint

--- src/main/java/com/webank/wecross/network/rpc/handler/XATransactionHandler.java
+++ src/main/java/com/webank/wecross/network/rpc/handler/XATransactionHandler.java
@@ -46,13 +46,10 @@ public class XATransactionHandler implements URIHandler {
 
         } catch (JsonParseException e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
         } catch (JsonMappingException e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
         } catch (IOException e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
         }
     }
 }
