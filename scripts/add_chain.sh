ca5404f2d59ceb96ac20eb643cb7eaf26bc346f2
ywy2090 <912554887@qq.com>
2020-07-28 14:28:34 +0800

    update stub-sample.toml (#316)
    * update stub-sample.toml
    
    * add gm account/chain prompt

--- scripts/add_chain.sh
+++ scripts/add_chain.sh
@@ -8,12 +8,13 @@ help() {
     echo $1
     cat << EOF
 Usage:
-    -t <type>                           [Required] type of chain, BCOS2.0 or Fabric1.4
+    -t <type>                           [Required] type of chain, BCOS2.0 or GM_BCOS2.0 or Fabric1.4
     -n <name>                           [Required] name of chain
     -d <dir>                            [Optional] generated target_directory, default conf/chains/
     -h                                  [Optional] Help
 e.g
     bash $0 -t BCOS2.0 -n my_bcos_chain
+    bash $0 -t GM_BCOS2.0 -n my_gm_bcos_chain
     bash $0 -t Fabric1.4 -n my_fabric_chain
 EOF
 
