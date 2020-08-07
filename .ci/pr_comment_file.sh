7a1370d2cc53ac5dc670ef51cc74f403ed6ae583
JimmyShi22 <417711026@qq.com>
2020-08-07 19:10:00 +0800

    test5

--- .ci/pr_comment_file.sh
+++ .ci/pr_comment_file.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+LOG_INFO()
+{
+    echo -e "\033[32m[INFO] $@\033[0m"
+}
+
+LOG_ERROR()
+{
+    echo -e "\033[31m[ERROR] $@\033[0m"
+}
+
 pr_comment_file()
 {
     local content="$(cat ${1}|sed ':label;N;s/\n/\\n/g;b label')"
@@ -14,6 +24,7 @@ pr_comment_file_github()
     local pr_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
     LOG_INFO "PR: ${pr_number}"
     LOG_INFO "GITHUB_REPOSITORY: ${GITHUB_REPOSITORY}"
+    LOG_INFO "TOKEN: " ${GITHUB_TOKEN}
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"${content}\"}" "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${pr_number}/comments"
 }
 
