eea41de86b32f83f834e53faca0e8c95b78b21a5
shareong <740310627@qq.com>
2020-08-14 14:34:11 +0800

    <fix>(connectionProperties): not set properties for remote connection when no local connection found

--- src/main/java/com/webank/wecross/zone/ChainInfo.java
+++ src/main/java/com/webank/wecross/zone/ChainInfo.java
@@ -109,6 +109,10 @@ public class ChainInfo {
                 + ", stubType='"
                 + stubType
                 + '\''
+                + ", resources="
+                + resources
+                + ", properties="
+                + properties
                 + ", checksum='"
                 + checksum
                 + '\''
