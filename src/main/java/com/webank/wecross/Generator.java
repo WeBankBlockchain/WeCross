13788c17b01b7e82e0f1153bcbe2bd5cbdecf3a1
yangzan <yangzan@andlinks.cin>
2020-09-07 14:52:12 +0800

    提交spring注解上下文的成员变量化以及日志打印改造

--- src/main/java/com/webank/wecross/Generator.java
+++ src/main/java/com/webank/wecross/Generator.java
@@ -3,24 +3,28 @@ package com.webank.wecross;
 import com.webank.wecross.config.StubManagerConfig;
 import com.webank.wecross.stub.StubFactory;
 import com.webank.wecross.stubmanager.StubManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Generator {
-
+    private static final Logger logger  = LoggerFactory.getLogger(Generator.class);
+    private static final int ARGS_LENGTH = 3;
+    private static ApplicationContext context;
     public static void main(String[] args) {
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext(StubManagerConfig.class);
 
-        if (args.length < 3) {
-            System.out.println("Usage: connection/account <type> <path> <args>");
+        context = new AnnotationConfigApplicationContext(StubManagerConfig.class);
+
+        if (args.length < ARGS_LENGTH) {
+            logger.debug("Usage: connection/account <type> <path> <args>");
             return;
         }
 
         String op = args[0];
         String type = args[1];
         String path = args[2];
-        System.out.println("operator: " + op + " type: " + type + " path: " + path);
+        logger.debug("operator:{} type:{} path:{}", op, type, path);
 
         StubManager stubManager = context.getBean(StubManager.class);
         try {
@@ -31,10 +35,10 @@ public class Generator {
             } else if (op.equals("account")) {
                 stubFactory.generateAccount(path, new String[] {});
             } else {
-                System.err.println("Unknown operation: " + op);
+                logger.error("Unknown operation: " + op);
             }
         } catch (Exception e) {
-            System.err.println("Error" + e.toString());
+            logger.error("Error" + e.toString());
             return;
         }
     }
