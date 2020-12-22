6227e14a1a32ff46eed1cc0673daaa511b166c1c
shareong <740310627@qq.com>
2020-12-22 17:43:35 +0800

    <fix>(xa && transaction): use admin account to query xa info && add error message in transaction

--- src/main/java/com/webank/wecross/stub/TransactionResponse.java
+++ src/main/java/com/webank/wecross/stub/TransactionResponse.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TransactionResponse {
-    private Integer errorCode = 0;
+    private int errorCode = 0;
     private String message = new String();
     private String hash = new String();
     private List<String> extraHashes;
