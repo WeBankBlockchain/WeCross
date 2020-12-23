2d4fcd033747885ab4d45128672e72c06e07483c
shareong <740310627@qq.com>
2020-12-23 14:30:57 +0800

    expect

--- .ci/ci_prepare_mysql_mac.sh
+++ .ci/ci_prepare_mysql_mac.sh
@@ -14,11 +14,21 @@ cd .
 
 brew services start mysql
 
-mysql_secure_installation <<EOF
-123456
-123456
-n
-n
-n
-n
+expect <<EOF
+spawn mysql_secure_installation 2>/dev/null
+expect "Press y|Y for Yes, any other key for No:"
+send "n\r"
+expect "New password:"
+send "${DB_PASSWORD}\r"
+expect "Re-enter new password:"
+send "${DB_PASSWORD}\r"
+expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
+send "n\r"
+expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
+send "n\r"
+expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
+send "n\r"
+expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
+send "n\r"
+expect eof
 EOF
