27a1ecb27d2451c1f539aaa85dee5d851b1cb838
shareong <740310627@qq.com>
2020-12-23 11:19:13 +0800

    <project>(git action): add checks for mac

---
+++ .ci/ci_prepare_mysql_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DB_PASSWORD=${CI_DB_PASSWORD}
+
+brew install mysql
+
+brew services start mysql
+
+if [ ! ${DB_PASSWORD} ]; then
+    DB_PASSWORD='123456'
+fi
+
+mysql_secure_installation <<EOF
+    N
+    ${DB_PASSWORD}
+    ${DB_PASSWORD}
+    N
+    N
+    N
+    N
+EOF
+
+brew services restart mysql
