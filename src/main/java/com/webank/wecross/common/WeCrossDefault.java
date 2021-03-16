5231044b6812e1dfaf2fa83c53bd594cdadd7ac1
JimmyShi22 <417711026@qq.com>
2021-03-16 15:01:56 +0800

    update version

--- src/main/java/com/webank/wecross/common/WeCrossDefault.java
+++ src/main/java/com/webank/wecross/common/WeCrossDefault.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class WeCrossDefault {
-    public static final String VERSION = "v1.1.0";
+    public static final String VERSION = "v1.1.1";
 
     public static final String TEMPLATE_URL = "http://127.0.0.1:8080/";
 
